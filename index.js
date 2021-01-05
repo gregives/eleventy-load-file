465fca98669a073eb50433a1a64ddadddd34d7cc
Greg Ives <gregives774@gmail.com>
2021-01-05 00:26:48 +0000

    Remove destructure of options

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const path = require("path");
 
-module.exports = function (content, options = {}) {
-  const { name, publicPath, outputPath } = {
-    ...{
-      publicPath: this.config.outputDir,
-      outputPath: "assets",
-      name: "[hash].[ext]",
-    },
-    ...options,
+module.exports = function (content, _options) {
+  const options = {
+    publicPath: this.config.outputDir,
+    outputPath: "assets",
+    name: "[hash].[ext]",
+    ..._options,
   };
-  const filepath = path.posix.join(publicPath, outputPath, name);
+  const filepath = path.posix.join(
+    options.publicPath,
+    options.outputPath,
+    options.name
+  );
   const route = path.posix.relative(
-    publicPath,
+    options.publicPath,
     this.emitFile(content, filepath)
   );
   return path.posix.join("/", route);
